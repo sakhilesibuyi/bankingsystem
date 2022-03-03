06d8894adc33c99d47f14109ccd541aa95cdd51f
sakhilesibuyi <sakhile.sibuyi1@gmail.com>
2022-03-03 11:11:02 +0200

    adding Login view url

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
-from .views import RegisterView
-# , LoginView, UserView, LogoutView
+from .views import RegisterView, LoginView
+# , UserView, LogoutView
 
 from django.urls import path
 
 urlpatterns = [
-    path('register',RegisterView.as_view())
+    path('register',RegisterView.as_view()),
+    path('login',LoginView.as_view()),
 ]
