1afdecbdae18a2e0083606808f9af20e718b9441
sakhilesibuyi <sakhile.sibuyi1@gmail.com>
2022-03-03 16:58:27 +0200

    add transfer, deposit and account models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from decimal import MAX_EMAX
+from pyexpat import model
+from statistics import mode
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -22,11 +24,31 @@ class Branch(models.Model):
 class Bank(models.Model):
     bank_name = models.CharField(max_length=250)
     branch = models.ForeignKey(Branch,on_delete=models.CASCADE, related_name="branch")
+    def __str__(self):
+        return "{} - {}".format(self.bank_name, self.branch)
 
 class ClientConsultant(models.Model):
     name = models.CharField(max_length=250)
+    def __str__(self):
+        return self.name
 
 class Client(models.Model):
     name = models.CharField(max_length=250)
     client_Address = models.CharField(max_length=250)
+    def __str__(self):
+        return self.name
 
+class Account(models.Model):
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="account_owner")
+    account_type = models.CharField(max_length=250)
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE, related_name="bank")
+    def __str__(self):
+        return "{} - {} - {}".format(self.owner, self.bank, self.account_type)
+
+class Transfer(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="acc")
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name="trans_branch")
+
+class Deposit(models.Model):
+    amount = models.FloatField()
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
