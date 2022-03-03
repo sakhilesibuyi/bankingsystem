868353383b6530439dc03fd1aa9a4841a2bff771
sakhilesibuyi <sakhile.sibuyi1@gmail.com>
2022-03-03 13:18:40 +0200

    adding branch and bank models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+from decimal import MAX_EMAX
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -10,3 +12,13 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+class Branch(models.Model):
+    branch_name = models.CharField(max_length=250)
+    branch_code = models.CharField(max_length=250)
+    def __str__(self):
+        return self.branch_name
+
+class Bank(models.Model):
+    bank_name = models.CharField(max_length=250)
+    branch = models.ForeignKey(Branch,on_delete=models.CASCADE)
