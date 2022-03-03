c9f4099c0a43ffd7d6902f5481939cef0d2e41b9
sakhilesibuyi <sakhile.sibuyi1@gmail.com>
2022-03-03 11:09:34 +0200

    adding Login View

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+from os import access
+import re
+from urllib import response
 from django.core import exceptions
 from django.shortcuts import render
 from rest_framework import serializers
@@ -7,6 +10,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 import jwt, datetime
+
+from .models import User
 from .serializers import UserSerializer
 
 # Create your views here.
@@ -17,3 +22,27 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginView(APIView):
+    
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed("User Not Found")
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect Password Entered!")
+        payload = {
+            'id': user.id,
+             'exp': datetime.datetime.now() + datetime.timedelta(minutes=60),
+             'iat': datetime.datetime.now()
+        }
+        token = jwt.encode(payload,'secret',algorith='HS256')
+        response = Response()
+        response.set_cookie(key='token', value=token,httponly=True)
+        response.data = {
+            'token': token
+        }
+        return response
