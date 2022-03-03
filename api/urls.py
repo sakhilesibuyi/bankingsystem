f1ab2ef5556aa0ba31ab5ebf2d9c3a9c6afe87ab
sakhilesibuyi <sakhile.sibuyi1@gmail.com>
2022-03-03 21:53:06 +0200

    added client url

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 
+from unicodedata import name
 from rest_framework.authtoken.views import obtain_auth_token
 
 
@@ -9,17 +10,21 @@ from .views import (
     BranchView,
     BankView,
      RegisterView,
-     LogOutView
+     LogOutView,
+     ClientView
      )
 
+# url patterns to be matched
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('register',RegisterView.as_view()),
     path('login',obtain_auth_token, name='api_token_auth'),
-    path('logout',LogOutView.as_view()),
-    path('branches/',BranchView.as_view()),
+    path('logout',LogOutView.as_view(), name="logout-view"),
+    path('branches/',BranchView.as_view(), name="branch-lists"),
+    path('bank/', BankView.as_view(), name="bank-lists"),
+    path('client/',ClientView.as_view(), name="client-lists"),
+    re_path(r'^client/(?P<pk>[0-9]+)/', ClientView.as_view(),name="client-detail"), # gets matched when you include the id in the url, the id is expected to be a numeric value
     re_path(r'^bank/(?P<pk>[0-9]+)/', BankDetailView.as_view(), name="bank-detail"),
     re_path(r'^branch/(?P<pk>[0-9]+)/', BranchDetailView.as_view(), name="branch-detail"),
-    path('bank/', BankView.as_view()),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 
 ]
